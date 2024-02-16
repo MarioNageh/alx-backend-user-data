6e2a58c10411685bd415fa39ebf0be84f2cb1631
Mario Nageh <marionageh7@gmail.com>
2024-02-16 12:21:23 +0200

    To Task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of auth
 """
 import base64
+from typing import TypeVar
 
 from api.v1.auth.auth import Auth
 from models.user import User
@@ -66,3 +67,31 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method that require current user.
+        """
+        authorization_header = self.authorization_header(request)
+        if not authorization_header:
+            return None
+
+        base64_authorization_header = \
+            self.extract_base64_authorization_header(authorization_header)
+        if not base64_authorization_header:
+            return None
+
+        decoded_base64_authorization_header = \
+            self.decode_base64_authorization_header(
+                base64_authorization_header)
+        if not decoded_base64_authorization_header:
+            return None
+
+        user_credentials = self.extract_user_credentials(
+            decoded_base64_authorization_header)
+        if not user_credentials:
+            return None
+
+        user = self.user_object_from_credentials(
+            user_credentials[0], user_credentials[1])
+        return user
