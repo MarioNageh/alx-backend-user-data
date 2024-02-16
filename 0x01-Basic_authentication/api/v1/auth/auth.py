88d30fda5e3511a1c7a3f5f182fc8c3b2d456871
Mario Nageh <marionageh7@gmail.com>
2024-02-16 12:07:22 +0200

    Basic Auth Extract Base64 Part

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,6 +8,8 @@ from typing import List, TypeVar
 class Auth:
     """Auth class."""
 
+    AUTH_KEY: str = ""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Method that require authentication.
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class.
     """
-    ...
+    AUTH_KEY = "Basic "
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extract base64 authorization header
+        """
+        if isinstance(authorization_header, str) and \
+                authorization_header.startswith(self.AUTH_KEY):
+            return authorization_header[len(self.AUTH_KEY):]
