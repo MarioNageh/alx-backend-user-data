e560641bf186f87d7ba3c3d8b7044df6d9328056
Mario Nageh <marionageh7@gmail.com>
2024-02-16 15:55:06 +0200

    Task 9

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" Module of auth
+"""
+Module of auth
+SessionAuth class.
 """
 import uuid
 from typing import TypeVar
@@ -14,7 +16,11 @@ class SessionAuth(Auth):
     user_id_by_session_id: dict = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Create a session."""
+        """
+        Create a session.
+        :param user_id:
+        :return:
+        """
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
@@ -22,12 +28,21 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Return the User ID by requesting it."""
+        """
+        Return the User ID by requesting it.
+        :param session_id:
+        :return:
+        """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Method that require current user.
+        :param request:
+        :return:
+        """
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
 
