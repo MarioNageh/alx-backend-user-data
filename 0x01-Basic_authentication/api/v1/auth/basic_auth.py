6f0b5aa2438e54c93de581cbd0f9e100e8c94f7b
Mario Nageh <marionageh7@gmail.com>
2024-02-16 12:18:35 +0200

    To Task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ Module of auth
 """
-from flask import request
-from typing import List, TypeVar
-import os
+import base64
 
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -20,3 +19,50 @@ class BasicAuth(Auth):
         if isinstance(authorization_header, str) and \
                 authorization_header.startswith(self.AUTH_KEY):
             return authorization_header[len(self.AUTH_KEY):]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+    ) -> str:
+        """ Decode base64 authorization header
+        """
+        if isinstance(base64_authorization_header, str):
+            try:
+                return base64.b64decode(
+                    base64_authorization_header.encode('utf-8')) \
+                    .decode('utf-8')
+            except Exception:
+                pass
+        return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """ Extract user credentials
+        """
+        if isinstance(decoded_base64_authorization_header, str) and \
+                ":" in decoded_base64_authorization_header:
+            return tuple(decoded_base64_authorization_header.split(":", 1))
+        return (None, None)
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+    ) -> Auth:
+        """ User object from credentials
+        """
+        if not isinstance(user_email, str) and not isinstance(user_pwd, str):
+            return None
+
+        try:
+            user = User.search({"email": user_email})
+            if user:
+                if user[0].is_valid_password(user_pwd):
+                    return user[0]
+            else:
+                return None
+        except Exception:
+            return None
+        return None
